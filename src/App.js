94f69ae46de7100ebd0e1a0e891f320e6973bb57
JansenDF <jansen.lima.df@gmail.com>
2022-09-26 15:46:55 -0300

    Add functions state with name and count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Jansen Lima",
+      cont: 0,
+    };
+  }
+
+  handleSetName = () => {
+    this.setState({ name: "Jão" });
+  };
+
+  handleSetCont = (event) => {
+    event.preventDefault();
+    const { cont } = this.state;
+    this.setState({ cont: cont + 1 });
+  };
+  render() {
+    const { name, cont } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handleSetName}>
+            {name} {cont}
+          </p>
+          <a
+            onClick={this.handleSetCont}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
